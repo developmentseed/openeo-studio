e449adb5fcf0592f668c34f27ce5fac42a746139
Alice Rühl <alice.ruehl@gmail.com>
2026-01-19 14:29:43 +0100

    Remove hand-drawn styles

--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -4,10 +4,6 @@ import { createColorPalette } from './color-palette';
 const config = defineConfig({
   theme: {
     tokens: {
-      fonts: {
-        body: { value: '"Gloria Hallelujah", sans-serif' },
-        heading: { value: '"Gloria Hallelujah", serif' }
-      },
       colors: {
         primary: createColorPalette('#1E7BC6'),
         secondary: createColorPalette('#5FAD56'),
@@ -17,14 +13,6 @@ const config = defineConfig({
         success: createColorPalette('#5FAD56'),
         info: createColorPalette('#1E7BC6'),
         surface: createColorPalette('#FFF')
-      }
-    },
-    layerStyles: {
-      handDrawn: {
-        description: 'A hand-drawn style with irregular borders',
-        value: {
-          borderRadius: '255px 15px 225px 15px/15px 225px 15px 255px'
-        }
       }
     }
   }
