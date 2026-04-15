26f248f753b3c0e1481b6ccf115be528e90833f3
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2026-04-15 14:53:10 +0200

    refactor: Replace IconButton with Button for services panel and update icon component in LayerControl

--- app/components/layout/app-header.tsx
+++ app/components/layout/app-header.tsx
@@ -1,20 +1,11 @@
 import { useState } from 'react';
-import { Button, Flex, Heading, IconButton, Separator } from '@chakra-ui/react';
+import { Button, Flex, Heading, Separator } from '@chakra-ui/react';
 import { useNavigate, useLocation } from 'react-router';
 import { useAuth } from 'react-oidc-context';
 import { APP_TITLE } from '$config/constants';
 import { UserInfo } from '$components/auth/user-info';
 import { ServicesPanel } from '$components/layout/services-panel';
 import SmartLink from '$utils/smart-link';
-
-function ServicesIcon() {
-  return (
-    <svg viewBox='0 0 16 16' width='16' height='16' fill='currentColor'>
-      <path d='M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z' />
-      <path d='M5 4h6v1H5V4zm0 3h6v1H5V7zm0 3h4v1H5v-1z' />
-    </svg>
-  );
-}
 
 export function AppHeader() {
   const navigate = useNavigate();
@@ -59,14 +50,13 @@ export function AppHeader() {
           Documentation
         </Button>
         {isAuthenticated && (
-          <IconButton
-            aria-label='Permanent services'
-            size='sm'
+          <Button
             variant='ghost'
+            size='sm'
             onClick={() => setServicesPanelOpen(true)}
           >
-            <ServicesIcon />
-          </IconButton>
+            My Services
+          </Button>
         )}
         <Separator orientation='vertical' height='8' />
         <UserInfo />
