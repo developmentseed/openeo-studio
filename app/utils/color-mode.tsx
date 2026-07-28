6130ffc6f4b899b3d690bcb2e72b078661df2d54
Daniel da Silva <danielfdsilva@gmail.com>
2026-07-28 09:58:51 +0100

    Prepare colormode option

---
+++ app/utils/color-mode.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import * as React from 'react';
+import {
+  ClientOnly,
+  IconButton,
+  Skeleton,
+  Span,
+  IconButtonProps,
+  SpanProps
+} from '@chakra-ui/react';
+import { ThemeProvider, useTheme, ThemeProviderProps } from 'next-themes';
+import { LuMoon, LuSun } from 'react-icons/lu';
+
+export function ColorModeProvider(props: ThemeProviderProps) {
+  return (
+    <ThemeProvider attribute='class' disableTransitionOnChange {...props} />
+  );
+}
+
+export type ColorMode = 'light' | 'dark';
+
+export interface UseColorModeReturn {
+  colorMode: ColorMode;
+  setColorMode: (colorMode: ColorMode) => void;
+  toggleColorMode: () => void;
+}
+
+export function useColorMode(): UseColorModeReturn {
+  const { resolvedTheme, setTheme, forcedTheme } = useTheme();
+  const colorMode = forcedTheme || resolvedTheme;
+  const toggleColorMode = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+  };
+  return {
+    colorMode: colorMode as ColorMode,
+    setColorMode: setTheme,
+    toggleColorMode
+  };
+}
+
+export function useColorModeValue<T>(light: T, dark: T) {
+  const { colorMode } = useColorMode();
+  return colorMode === 'dark' ? dark : light;
+}
+
+export function ColorModeIcon() {
+  const { colorMode } = useColorMode();
+  return colorMode === 'dark' ? <LuMoon /> : <LuSun />;
+}
+
+export function ColorModeButton(props: Omit<IconButtonProps, 'aria-label'>) {
+  const { toggleColorMode } = useColorMode();
+  return (
+    <ClientOnly fallback={<Skeleton boxSize='9' />}>
+      <IconButton
+        onClick={toggleColorMode}
+        variant='ghost'
+        aria-label='Toggle color mode'
+        size='sm'
+        {...props}
+      >
+        <ColorModeIcon />
+      </IconButton>
+    </ClientOnly>
+  );
+}
+
+export function LightMode(props: SpanProps) {
+  return (
+    <Span
+      color='fg'
+      display='contents'
+      className='chakra-theme light'
+      colorPalette='gray'
+      colorScheme='light'
+      {...props}
+    />
+  );
+}
+
+export function DarkMode(props: SpanProps) {
+  return (
+    <Span
+      color='fg'
+      display='contents'
+      className='chakra-theme dark'
+      colorPalette='gray'
+      colorScheme='dark'
+      {...props}
+    />
+  );
+}
